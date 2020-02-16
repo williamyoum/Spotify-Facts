50282499a5ff153839b5271a332e0f1a555bbdc4
Will <will.youm@gmail.com>
2020-02-16 00:42:43 -0500

    implement App.test react-tester

--- client/src/components/App.test.js
+++ client/src/components/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import render from 'react-test-renderer'
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
@@ -8,3 +9,10 @@ it("renders without crashing", () => {
   ReactDOM.unmountComponentAtNode(div);
 })
 
+// test('renders a message', () => {
+//   const { container, getByText } = render(<Greeting />)
+//   expect(getByText('Hello, world!')).toBeInTheDocument()
+//   expect(container.firstChild).toMatchInlineSnapshot(`
+//     <h1>Hello, World!</h1>
+//   `)
+// })
